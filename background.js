c1053aff3449ca33e9ff95b9cfd3d826ce683602
AhnJB <84900093+AhnJB@users.noreply.github.com>
2023-02-04 16:24:14 +0900

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-const images = ["0.jpg","1.jpg","2.jpg"]
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImages = document.createElement("img");
-
-bgImages.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImages);
